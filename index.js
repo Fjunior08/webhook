066282b1998494f87dfbdf82c14eaf898f144922
Pecista - Distribuição e Representação de Autopeças <tecnologia.pecista@gmail.com>
2022-05-24 09:06:16 -0300

    Commit"heroku"

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-const User = require('./src/routes');
-const http = require('http');
- 
-const server = http.createServer((req, res) => {
-    console.log('server rodando!')
-})
+const app = require('./src/app');
 
-server.listen(8080);
+const PORT = process.env.PORT || 8080;
 
-// const app = require('./src/app');
-
-// const PORT = process.env.PORT || 8080;
-
-// app.listen(PORT, () => {console.log(`App listenner in port ${PORT}`)})
+app.listen(PORT, () => {console.log(`App listenner in port ${PORT}`)})
